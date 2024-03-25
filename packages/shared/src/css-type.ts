b7b331a6550f7442021ae330639c2895b3d8aa7d
mengdaoshizhongxinyang <programmercyk@gmail.com>
2024-03-25 18:00:32 +0800

    fix: stylex type support

--- packages/shared/src/css-type.ts
+++ packages/shared/src/css-type.ts
@@ -1,37 +1,31 @@
 import * as CSS from 'csstype'
 
 export type CSSProperties = CSS.PropertiesFallback<number | string>
+
+type CSSPropertiesKey = (keyof CSSProperties) | CSS.AtRules | CSS.Pseudos | (string & {})
+
+type SelectOrAtRules = CSS.AtRules | CSS.Pseudos | (string & {})
+
+type CSSPropertiesMoreValue<T> = {
+  default: T
+} & Partial<Record<SelectOrAtRules, T>>
+
 export type CSSPropertiesWithMultiValues = {
-  [K in keyof CSSProperties]:
-  | CSSProperties[K]
-  | ReadonlyArray<Extract<CSSProperties[K], string>>
+  [K in CSSPropertiesKey]?:
+  K extends keyof CSSProperties
+    ? (CSSProperties[K] | CSSPropertiesMoreValue<CSSProperties[K]>)
+    : CSSPropertiesWithMultiValues | (string & {})
 }
 
 export interface CSSObject
-  extends CSSPropertiesWithMultiValues,
-  // eslint-disable-next-line no-use-before-define
-  CSSPseudos,
-  // eslint-disable-next-line no-use-before-define
-  CSSOthersObject { }
-
-// eslint-disable-next-line no-unused-vars
-export type CSSPseudos = { [K in CSS.Pseudos]?: CSSObject }
-
-export interface ArrayCSSInterpolation
-  // eslint-disable-next-line no-use-before-define
-  extends ReadonlyArray<CSSInterpolation> { }
+  extends CSSPropertiesWithMultiValues { }
 
 export type InterpolationPrimitive =
-    | null
-    | undefined
-    | boolean
-    | number
-    | string
-    | CSSObject
+  | null
+  | undefined
+  | boolean
+  | number
+  | string
+  | CSSObject
 
-export interface CSSOthersObject {
-  // eslint-disable-next-line no-use-before-define
-  [propertiesName: string]: CSSInterpolation
-}
-
-export type CSSInterpolation = InterpolationPrimitive | ArrayCSSInterpolation
+export type CSSInterpolation = InterpolationPrimitive
