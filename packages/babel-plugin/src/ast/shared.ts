e56adf42733ddf6dd29c50277dbbd6187cc514a7
kanno <812137533@qq.com>
2024-05-07 07:31:24 +0800

    chore: layout

---
+++ packages/babel-plugin/src/ast/shared.ts
@@ -0,0 +1,25 @@
+import { types } from '@babel/core'
+import type { NodePath } from '@babel/core'
+
+export type StringLikeKindPath = NodePath<types.StringLiteral | types.Identifier> 
+
+export type CalleeExpression = types.Expression | types.V8IntrinsicIdentifier
+
+export function isStringLikeKind(path: NodePath<types.Node>): path is StringLikeKindPath {
+  return path.isStringLiteral() || path.isIdentifier()
+}
+
+export function getStringLikeKindValue(path: StringLikeKindPath) {
+  if (path.node.type === 'StringLiteral') {
+    return path.node.value
+  }
+  return path.node.name
+}
+
+export function callExpression(callee: CalleeExpression, args: types.Expression[]) {
+  return types.callExpression(callee, args)
+}
+
+export function arrowFunctionExpression(params: types.Identifier[], body: types.Expression) {
+  return types.arrowFunctionExpression(params, body)
+}
