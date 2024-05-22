705684158e95dd908e1421f9991d540abafb4b48
mengdaoshizhongxinyang <programmercyk@gmail.com>
2024-05-22 14:53:24 +0800

    fix: react runtime type error

--- packages/react/src/jsx-namespace.d.ts
+++ packages/react/src/jsx-namespace.d.ts
@@ -4,6 +4,16 @@ import type { StylexProperty } from '@stylex-extend/shared'
 type ReactJSXLibraryManagedAttributes<C, P> = JSX.LibraryManagedAttributes<C, P>
 type ReactJSXIntrinsicElements = JSX.IntrinsicElements
 
+type ReactJSXElement = JSX.Element
+type ReactJSXElementClass = JSX.ElementClass
+type ReactJSXElementAttributesProperty = JSX.ElementAttributesProperty
+type ReactJSXElementChildrenAttribute = JSX.ElementChildrenAttribute
+type ReactJSXLibraryManagedAttributes<C, P> = JSX.LibraryManagedAttributes<C, P>
+type ReactJSXIntrinsicAttributes = JSX.IntrinsicAttributes
+type ReactJSXIntrinsicClassAttributes<T> = JSX.IntrinsicClassAttributes<T>
+type ReactJSXIntrinsicElements = JSX.IntrinsicElements
+type ReactJSXElementType = string | React.JSXElementConstructor<any>
+
 export namespace StyledJSX {
   export type LibraryManagedAttributes<C, P> = { stylex?: StylexProperty } & ReactJSXLibraryManagedAttributes<C, P>
 
@@ -12,4 +22,15 @@ export namespace StyledJSX {
       stylex?: StylexProperty
     }
   }
+
+  export type ElementType = ReactJSXElementType
+  export interface Element extends ReactJSXElement {}
+  export interface ElementClass extends ReactJSXElementClass {}
+  export interface ElementAttributesProperty
+    extends ReactJSXElementAttributesProperty {}
+  export interface ElementChildrenAttribute extends ReactJSXElementChildrenAttribute {}
+
+  export interface IntrinsicAttributes extends ReactJSXIntrinsicAttributes {}
+  export interface IntrinsicClassAttributes<T>
+    extends ReactJSXIntrinsicClassAttributes<T> {}
 }
