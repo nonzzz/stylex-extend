fdf75459aec93d0cc36eb7c9b795c66046f7b768
kanno <812137533@qq.com>
2024-05-30 12:42:13 +0800

    chore: init

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,6 +4,21 @@ export function injectGlobalStyle(..._: Array<Record<string, StylexCSS>>): strin
   throw new Error("'injectGlobalStyle' calls should be compiled away.")
 }
 
-export function inline(_: CSSObject): any {
+export function inline(_: CSSObject) {
   throw new Error("'inline' calls should be compiled away.")
+}
+
+function createWhenAPI(errorMessage: string) {
+  return (selector: string, pseudo?: string) => {
+    throw new Error(errorMessage)
+  }
+}
+
+export const when = {
+  // a b
+  ancestor: createWhenAPI("'when.ancestor' calls should be compiled away."),
+  // a:has(b)
+  descendent: createWhenAPI("'when.descendent' calls should be compiled away."),
+  // a + b
+  sibling: createWhenAPI("'when.sibling' calls should be compiled away.")
 }
