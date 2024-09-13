a6a4da4e156faf73af7332eb92c6e47d28e54cf7
kanno <812137533@qq.com>
2024-09-13 14:12:16 +0800

    x

---
+++ packages/babel-plugin/src/visitor/when.ts
@@ -0,0 +1,20 @@
+// reference: https://github.com/facebook/stylex/issues/536
+// TL;DR: I have not plan to implement the same api in stylex-extend
+// Currently support API's
+// - ancestor (a b)
+// - descendent (a:has(b))
+// - sibling (a + b)
+
+/**
+ *  import { create, props } from '@stylexjs/stylex'
+ *  const styles  = {
+ *   container: {}
+ * }
+ *  <div />
+ */
+
+function uuId() {
+  // 
+}
+
+export function transformWhen() {}
