ca3b9baf9d8e8f9e3809ffb9de45573b47701cb2
kanno <812137533@qq.com>
2025-01-14 18:41:31 +0800

    chore: init when

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -19,3 +19,18 @@ export function inline(_: CSSObject): StylexAttrsParamter {
 export function id(_?: string): string {
   throw new Error('`id` calls should be compiled away.')
 }
+
+function createWhenAPI(errorMessage: string) {
+  return (selector: string, pseudo?: string) => {
+    throw new Error(errorMessage)
+  }
+}
+
+export const when = {
+  // a b
+  ancestor: createWhenAPI("'when.ancestor' calls should be compiled away."),
+  // a:has(b)
+  descendent: createWhenAPI("'when.descendent' calls should be compiled away."),
+  // a + b
+  sibling: createWhenAPI("'when.sibling' calls should be compiled away.")
+}
